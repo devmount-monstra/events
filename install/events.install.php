e0b6072f3dc3f0900fbd71756278de684cbc7caf
Andreas Müller <mail@devmount.de>
2016-01-06 12:51:43 +0100

    improved: install db fields

--- install/events.install.php
+++ install/events.install.php
@@ -4,28 +4,22 @@
 defined('MONSTRA_ACCESS') or die('No direct script access.');
 
 // Initialize Database
-Table::create(
-    'events',
-    array(
-        'title',
-        'timestamp',
-        'category',
-        'date',
-        'time',
-        'location',
-        'description',
-        'image',
-        'audio',
-        'color',
-    )
-);
-Table::create(
-    'categories',
-    array(
-        'title',
-        'color',
-    )
-);
+Table::create('events', array('title'));
+$e = new Table('events');
+$e->addField('timestamp');
+$e->addField('category');
+$e->addField('date');
+$e->addField('time');
+$e->addField('location');
+$e->addField('short');
+$e->addField('description');
+$e->addField('image');
+$e->addField('audio');
+$e->addField('color');
+
+Table::create('categories', array('title'));
+$c = new Table('categories');
+$c->addField('color');
 
 // Add Options
 // Option::add('toggle_duration', '400');
