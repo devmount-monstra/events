9e5049ed58a49315fd685cce7ddc92402352d8f8
devmount <mail@devmount.de>
2016-01-11 20:25:53 +0100

    added: uninstall option

--- install/events.uninstall.php
+++ install/events.uninstall.php
@@ -3,6 +3,9 @@
 // let only monstra allow to use this script
 defined('MONSTRA_ACCESS') or die('No direct script access.');
 
+// drop db tables
+// Table::drop('events');
+
 // Delete Options
 Option::delete('events_image_directory');
 Option::delete('events_audio_directory');
