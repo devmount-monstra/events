125f633c1eae70ce2790032a581726a512e5b8c0
Andreas Müller <mail@devmount.de>
2016-02-05 14:31:06 +0100

    added: views for different list types

--- views/frontend/index.view.php
+++
@@ -1,8 +0,0 @@
-<div class="events-plugin">
-	<div class='click'>
-        <h3><?php echo $click; ?></h3>
-        <div class='events'>
-            <?php echo $events; ?>
-        </div>
-    </div>
-</div>
---
+++ views/frontend/list-minimal.view.php
@@ -0,0 +1,21 @@
+<div class="events-plugin">
+    <div class="table-wrapper">
+        <table>
+            <tbody>
+            <?php if (sizeof($eventlist) > 0) {
+                foreach ($eventlist as $event) { ?>
+                    <tr>
+                        <td><?php echo $categories[$event['category']] ?></td>
+                        <td><?php echo $event['title'] == '' ? $event['short'] : '»' . $event['title'] . '«' ?></td>
+                        <td><?php echo $event['date'] ?></td>
+                    </tr>
+                <?php }
+            } else { ?>
+                <tr>
+                    <td colspan="3"><?php echo __('No events in this list view.', 'events'); ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
