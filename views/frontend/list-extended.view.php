b63ccfc6c26be1dd7b3a4f1d665fff7ffe0fe442
Andreas Müller <mail@devmount.de>
2016-02-18 09:26:18 +0100

    improved: extended list css hover

--- views/frontend/list-extended.view.php
+++ views/frontend/list-extended.view.php
@@ -3,42 +3,44 @@
 		foreach ($eventlist as $event) { ?>
 			<div class="row">
 				<div class="col-md-12 event">
-					<div
-						class="event-image section-<?php echo $event['imagesection']; ?>"
-						style="background-image: url(<?php echo $event['image']; ?>"
-					>
-					</div>
 					<div class="event-content" style="background: #<?php echo $event['color'] ? $event['color'] : $categories['color'][$event['category']]; ?>;">
-						<?php echo Html::heading($categories['title'][$event['category']], 3); ?>
-						<div class="description">
-							<?php echo $event['title'] == '' ? $event['short'] : '»' . $event['title'] . '«' ?><br />
-							<?php echo $event['location'] == '' ? '' : '@' . $event['location'] ?>
-						</div>
-						<div class="date">
-							<?php echo $event['date'] ?>
-						</div>
-					</div>
-                    <div class="event-content-extend">
-                        <div>
-							<?php echo
-								$event['date'] .
-								($event['time'] ? ' – ' . $event['time'] : '') .
-								($event['openat'] ? ', ' . __('Lounge ab ', 'events') . $event['openat'] : '')
-							?>
-						</div>
-                        <div><?php echo ($event['address'] ? $event['address'] : '') ?></div>
-                        <div><?php echo ($event['short'] ? $event['short'] : '') ?></div>
-                        <div>
-							<?php if ($event['facebook']) {
-								echo
-									Html::arrow('right') . ' ' . Html::anchor(__('Facebook', 'events'), 'https://www.facebook.com/events/' . $event['facebook'], array('class' => 'facebook', 'target' => '_blank'));
-							} ?>
-							<?php if ($event['address']) {
-								echo
-									Html::nbsp() . Html::nbsp() . Html::nbsp() .
-									Html::arrow('right') . ' ' . Html::anchor(__('Map', 'events'), 'http://nominatim.openstreetmap.org/search?q=' . $event['address'], array('class' => 'map', 'target' => '_blank'));
-							} ?>
-						</div>
+                        <div
+                            class="event-image section-<?php echo $event['imagesection']; ?>"
+                            style="background-image: url(<?php echo $event['image']; ?>"
+                        >
+                        </div>
+                        <div class="description">
+                            <?php echo Html::heading($categories['title'][$event['category']], 3); ?>
+                            <div class="text">
+                                <?php echo $event['title'] == '' ? $event['short'] : '»' . $event['title'] . '«' ?><br />
+                                <?php echo $event['location'] == '' ? '' : '@' . $event['location'] ?>
+                            </div>
+                            <div class="date">
+                                <?php echo $event['date'] ?>
+                            </div>
+                            <div class="event-content-extend">
+                                <div>
+                                    <?php echo
+                                        $event['date'] .
+                                        ($event['time'] ? ' – ' . $event['time'] : '') .
+                                        ($event['openat'] ? ', ' . __('Lounge ab ', 'events') . $event['openat'] : '')
+                                    ?>
+                                </div>
+                                <div><?php echo ($event['address'] ? $event['address'] : '') ?></div>
+                                <div><?php echo ($event['short'] ? $event['short'] : '') ?></div>
+                                <div>
+                                    <?php if ($event['facebook']) {
+                                        echo
+                                            Html::arrow('right') . ' ' . Html::anchor(__('Facebook', 'events'), 'https://www.facebook.com/events/' . $event['facebook'], array('class' => 'facebook', 'target' => '_blank'));
+                                    } ?>
+                                    <?php if ($event['address']) {
+                                        echo
+                                            Html::nbsp() . Html::nbsp() . Html::nbsp() .
+                                            Html::arrow('right') . ' ' . Html::anchor(__('Map', 'events'), 'http://nominatim.openstreetmap.org/search?q=' . $event['address'], array('class' => 'map', 'target' => '_blank'));
+                                    } ?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 				</div>
 			</div>
