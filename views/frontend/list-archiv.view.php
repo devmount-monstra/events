be45089fa53594eb18b4a9c119a582c4a1d8b9fe
Andreas Müller <mail@devmount.de>
2016-03-05 00:25:23 +0100

    improved: archiv list template

--- views/frontend/list-archiv.view.php
+++ views/frontend/list-archiv.view.php
@@ -2,7 +2,7 @@
 	<?php if (sizeof($eventlist) > 0) {
 		foreach ($eventlist as $event) { ?>
 			<div class="row">
-				<div class="col-md-12 event">
+				<div class="col-md-12 event archiv">
 					<div class="event-content" style="background: #<?php echo $event['color'] ? $event['color'] : $categories['color'][$event['category']]; ?>;">
                         <div
                             class="event-image section-<?php echo $event['imagesection']; ?>"
@@ -13,8 +13,8 @@
                             <?php echo Html::heading($categories['title'][$event['category']], 3); ?>
                             <div class="text">
                                 <?php echo $event['title'] == '' ? $event['short'] : '»' . $event['title'] . '«'; ?><br />
-                                <?php echo $event['date'] . ($event['time'] ? ' – ' . $event['time'] : ''); ?><br />
-                                <?php echo $event['location'] == '' ? '' : '@' . $event['location']; ?>
+                                <?php echo $event['date'] . ($event['time'] ? ' – ' . $event['time'] : ''); ?>
+                                <?php echo $event['location'] == '' ? '' : ' | @' . $event['location']; ?>
                                 <div style="white-space: pre-wrap;"><?php echo $event['archiv']; ?></div>
                             </div>
                         </div>
